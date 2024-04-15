3070937b3660f0aade17aa4ed597898c4f193e5d
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2024-04-15 13:24:21 +0530

    Minor logging tweak.

--- src/tendril/interests/mixins/graphs.py
+++ src/tendril/interests/mixins/graphs.py
@@ -33,12 +33,12 @@ class InterestGraphsMixin(InterestMixinBase):
 
             logger.debug(f"Searching for dashboard {dashboard_uid}")
             dashboard_info = await get_dashboard(dashboard_uid)
-            url = None
 
             if not dashboard_info:
-                logger.info(f"Creating dashboard {dashboard_uid}")
+                logger.info(f"Generating dashboard {dashboard_uid}")
                 payload = await generate_dashboard(spec)
                 payload.setdefault('uid', dashboard_uid)
+                logger.debug(f"Publishing dashboard {dashboard_uid}")
                 dashboard_info = await upsert_dashboard(
                     payload, team_id=team_id, folder_uid=folder_uid,
                     commit_msg="Generated from spec"
@@ -50,6 +50,7 @@ class InterestGraphsMixin(InterestMixinBase):
             if not url:
                 return {}
 
+            logger.debug(f"Constructing url params for '{dashboard_uid}'")
             params = {}
             for name, value in spec.variables_url.items():
                 params[f'var-{name}']= value
@@ -59,9 +60,13 @@ class InterestGraphsMixin(InterestMixinBase):
     async def graphs(self):
         if not self.graphs_specs:
             return {}
+        logger.debug("Determining Graphs Owner")
         owner = self._graphs_owner()
+        logger.debug("Determining grafana team_id")
         graphs_team_id = await ensure_graphs_team(interest_type=owner.type_name, interest_name=owner.name)
+        logger.debug("Determining grafana folder_id")
         folder_uid = await ensure_graphs_folder(interest_type=owner.type_name, interest_name=owner.name,
                                                 descriptive_name=owner.descriptive_name)
+        logger.debug("Determining embeddable graphs")
         response = await self._graphs_get(graphs_team_id, folder_uid)
         return response
